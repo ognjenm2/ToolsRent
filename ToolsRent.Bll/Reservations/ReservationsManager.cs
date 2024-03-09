ecc9d82c69b2456082df4433ee1bc6258a690a8f
ognjen.minic.enterp@gmail.com <ognjen.minic.enterp@gmail.com>
2024-03-09 13:50:00 +0100

    Add project files.

---
+++ ToolsRent.Bll/Reservations/ReservationsManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ToolsRent.Dal.Models;
+namespace ToolsRent.Bll.Reservations
+{
+    public class ReservationsManager
+    {
+        public static DTResult<ReservationsModel> GetActivities()
+        {
+            using (var db = new TestDBToolsReservationEntities())
+            {
+
+                Expression<Func<Activity, bool>> filter = null;
+                if (!String.IsNullOrWhiteSpace(param.Search.Value))
+                {
+                    filter = (p =>
+                            p.ActivityCode.Contains(param.Search.Value) ||
+                            p.Description.Contains(param.Search.Value)
+                        );
+                }
+                //
+                var data = AdministrationQueries.GetActivities(db, param.SortOrder, param.Start, param.Length, filter);
+                int count = AdministrationQueries.CountActivities(db, filter);
+                //
+                var result = new DTResult<CodeListModel>
+                {
+                    draw = param.Draw,
+                    data = AdministrationTranslator.Translate(data),
+                    recordsFiltered = count,
+                    recordsTotal = count
+                };
+
+                return result;
+            }
+        }
+    }
+}
---
+++ ToolsRent.Web/Controllers/ReservationsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToolsRent.Bll.Reservations;
+
+namespace ToolsRent.Web.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        // GET: Reservations
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ReservationsHandler( )
+        {
+            try
+            {
+                
+                var result = ReservationsManager.GetReservations( );
+
+                return Json(result);
+            }
+            catch (Exception e)
+            {
+                //ExceptionHelper.LogException(e);
+                return Json(new { error = e.Message });
+            }
+        }
+    }
+}
